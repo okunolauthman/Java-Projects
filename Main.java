136f2e642920a8947e368bde0a8eedeca8dc8e13
Uthman Okunola <127902774+okunolauthman@users.noreply.github.com>
2025-09-01 00:16:35 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,108 +1,37 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
 
 public class Main {
-
-    //Class to Create expense details (Blueprint & Layout)
-    static class Expense {
-
-        //Attributes to Store expense details
-        private final int id;
-        private final String category;
-        private final double amount;
-        private final String date;
-        private final String description;
-
-        //Constructor to initialize expense Object
-        public Expense(int id, String category, double amount, String date, String description) {
-            this.id = id;
-            this.category = category;
-            this.amount = amount;
-            this.date = date;
-            this.description = description;
-        }
-        // Overriding to return a string representation of the object
-        @Override
-        public String toString() {
-            return "Expense [id=" + id + ", category=" + category + ", amount=" + amount + ", date=" + date + ", description=" + description + "]";
-        }
-
-    }// ------------------------------ End of Class (Blueprint) -------------------------------------------------------------------------------------------------
-
-    // Main Expense tracker App
-
-    private static final ArrayList<Expense> expenses = new ArrayList<>();// Generic ArrayList that can store objects of type Expense.
-    private static int expenseId = 1; //Auto incremented ID for expense
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while (true) { // infinite loop till user exits
-            //Display Menu Options
-            System.out.println("\n Expense Tracker Menu:");
-            System.out.println("1. Add Expense");
-            System.out.println("2. View Expense");
-            System.out.println("3. Exit");
-            System.out.print("Enter your choice: ");
+        int[] numbers = {5, 3, 8, 4, 2,9,10,89,79};
+        System.out.println("Before sorting: " + Arrays.toString(numbers));
+        selectionSort(numbers);
+        System.out.println("After sorting: " + Arrays.toString(numbers));
+    }
 
-            int choice = sc.nextInt(); //Get User Input
-            sc.nextLine();
+    //---selection sort
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
 
-            switch (choice) {
-                case 1:
-                    addExpense(sc); //call method to add Expense
-                    break;
-                case 2:
-                    viewExpenses();// call method to view expense
-                    break;
-                case 3:
-                    System.out.println("Exiting Application. Goodbye!!!");
-                    sc.close();//close scanner before exiting
-                    return;// Exit Program
-                default:
-                    System.out.println("Invalid choice. Please Try Again");
+        // Outer loop:
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i; // Assume the first index is the smallest
+
+            // Inner loop: checks the next element and compares then swap
+            for (int j = i + 1; j < n; j++) { // J is the second index
+
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;  // Update to the new minimum's index
+                }
             }
-
+            // Swap the found minimum element with the first unsorted element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
         }
     }
-    //---------------------------------- End Of Main Method ------------------------------------------------------------------
-
-    // Method to add an Expense (Create Operation)
-    private static void addExpense(Scanner sc){
-        System.out.print("Enter category (e.g., Food, Transportation, Bills): ");
-        String category = sc.nextLine(); //Read category Input
-
-        System.out.print("Enter amount: ");
-        double amount = sc.nextDouble();
-        sc.nextLine();
-
-        System.out.print("Enter date (e.g., 2020-01-02): ");
-        String date = sc.nextLine();
-
-        System.out.print("Enter description: ");
-        String description = sc.nextLine();
-
-        // Create  and store the new Expense Object and add to the Arraylist
-        Expense newExpense = new Expense(expenseId++, category, amount, date, description);
-        expenses.add(newExpense); // The Generic ArrayList Stores the Data
-
-        System.out.println("Expense added successfully.");
-    }
-
-    // Method to View all recorded expenses (Read Operation)
-    private static void viewExpenses(){
-        if (expenses.isEmpty()) { // check the ArrayList Data Structure
-            System.out.println("No Expenses found or Recorded");
-        }
-        else {
-            System.out.println("\n List of Expenses");
-            for (Expense expense : expenses) {
-                System.out.println(expense);
-                //Prints everything in the Data Structure Arraylist
-            }
-        }
-    }
-
-
 }
 
 
